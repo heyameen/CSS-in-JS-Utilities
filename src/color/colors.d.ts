62e123a7968f9a5b220b48202683aab7e8e71190
ameen <aladeameen@gmail.com>
2024-08-10 11:22:30 +0100

    update config

---
+++ src/color/colors.d.ts
@@ -0,0 +1,5 @@
+export declare const toRgba: (color: string, alpha?: number) => string;
+export declare const lighten: (color: string, amount: number) => string;
+export declare const darken: (color: string, percentage: number) => string;
+export declare const complementary: (color: string) => string;
+export declare const analogous: (color: string, angle?: number) => string;
